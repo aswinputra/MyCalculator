5439b7a4f620b5ec415859acf4eaa7869da3f3f2
Aswin Hartono <aswinptr95@gmail.com>
2018-07-29 14:23:53 +0700

    first commit included with HTML and Script files

---
+++ script.js
@@ -0,0 +1,27 @@
+var app = angular.module("myApp",[]);
+app.controller("calculatorCtrl", calculatorCtrl);
+
+function calculatorCtrl(){
+	this.result = 0;
+	this.operatorClick = function(button){
+		this.operator = button;
+	};
+	this.calculate = function(){
+		var firstNum = parseFloat(this.number1);
+		var secondNum = parseFloat(this.number2);
+		switch(this.operator){
+			case '+':
+				this.result = firstNum + secondNum;
+				break;
+			case '-':
+				this.result = firstNum - secondNum;
+				break;
+			case '/':
+				this.result = firstNum / secondNum;
+				break;
+			case '*':
+				this.result = firstNum * secondNum;
+				break;
+		}
+	};
+};
